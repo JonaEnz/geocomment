2b0c193ec4d02307a6fa70131330de230c06cd04
SirYwell <hannesgreule@outlook.de>
2021-06-25 23:43:48 +0200

    Quarkus project setup

---
+++ backend/src/native-test/java/pls/change/that/NativeGreetingResourceIT.java
@@ -0,0 +1,9 @@
+package pls.change.that;
+
+import io.quarkus.test.junit.NativeImageTest;
+
+@NativeImageTest
+public class NativeGreetingResourceIT extends GreetingResourceTest {
+
+    // Execute the same tests but in native mode.
+}
