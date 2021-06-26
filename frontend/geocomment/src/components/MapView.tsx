8a49985182b30563bf4523d2d8de1c53f67b377e
J-Zoll <jonaszoll@gmx.de>
2021-06-26 13:49:14 +0200

    Add location access

--- frontend/geocomment/src/components/MapView.tsx
+++ frontend/geocomment/src/components/MapView.tsx
@@ -1,14 +1,30 @@
 import React from "react";
 import { useUserContext } from "../contexts/UserContext";
+import { location } from "../api/models/location";
 
 function MapView() {
+  const LOCATION_UPDATE_INTERVAL = 5000;
+
   const { userCredentials, setUserCredentials } = useUserContext();
+  const [location, setLocation] = React.useState<location>();
+
+  function updateLocation() {
+    navigator.geolocation.getCurrentPosition((position) => {
+      setLocation({
+        lat: position.coords.latitude,
+        lng: position.coords.longitude,
+      });
+    });
+  }
+
+  React.useEffect(() => {
+    setInterval(updateLocation, LOCATION_UPDATE_INTERVAL);
+  }, []);
 
   return (
     <div>
-      <h1>MapView</h1>
-      <h3>email: {userCredentials.email}</h3>
-      <h3>token: {userCredentials.token}</h3>
+      <p>Latitude: {location?.lat}</p>
+      <p>Longitude: {location?.lng}</p>
     </div>
   );
 }
