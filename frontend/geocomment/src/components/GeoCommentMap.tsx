f2a7562885187c7a79ccc7ba53a040f86330e7f5
J-Zoll <jonaszoll@gmx.de>
2021-06-26 17:45:49 +0200

    Add marker for current location

--- frontend/geocomment/src/components/GeoCommentMap.tsx
+++ frontend/geocomment/src/components/GeoCommentMap.tsx
@@ -1,4 +1,4 @@
-import { latLng } from "leaflet";
+import { Icon, latLng } from "leaflet";
 import React from "react";
 import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet'
 import { useLocation } from "../contexts/LocationContext";
@@ -6,8 +6,17 @@ import { setSourceMapRange } from "typescript";
 import { location } from "../api";
 import { LocationTracer } from "./LocationTracer";
 
+import nav_icon from '../icons/navigation_black_24dp.svg'
+
 export function GeoCommentMap() {
   const {location, setLocation} = useLocation()
+  const navigation_icon = new Icon({
+    iconUrl: nav_icon,
+    iconRetinaUrl: nav_icon,
+    iconAnchor: [30, 30],
+    popupAnchor: [30, 0],
+    iconSize: [60, 60],
+  })
   return (
     <MapContainer
         center={location}
@@ -22,6 +31,7 @@ export function GeoCommentMap() {
         attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
       />
+      <Marker position={location} icon={navigation_icon}/>
     </MapContainer>
   );
 }
