ff6250e49714b764e7f4d5498d1bdcd9e8d53e6e
bietz <ugixu@student.kit.edu>
2021-06-26 13:36:29 +0200

    Added ThreadRepository functionality

--- backend/src/main/java/pls/change/that/ThreadRepository.java
+++ backend/src/main/java/pls/change/that/ThreadRepository.java
@@ -7,4 +7,12 @@ import javax.enterprise.context.ApplicationScoped;
 @ApplicationScoped
 public class ThreadRepository implements PanacheRepository<Thread> {
 
+    public Thread findByTitle(String title) {
+        return find("title", title).firstResult();
+    }
+
+    public Thread findByLocation(Location location) {
+        return find("location", location).firstResult();
+    }
+
 }
