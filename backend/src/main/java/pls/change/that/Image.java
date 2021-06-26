4d0c6c27a46243da38b1c190df7744268bdc73d0
bietz <ugixu@student.kit.edu>
2021-06-26 21:03:26 +0200

    Added import

--- backend/src/main/java/pls/change/that/Image.java
+++ backend/src/main/java/pls/change/that/Image.java
@@ -3,6 +3,7 @@ package pls.change.that;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import java.awt.image.BufferedImage;
 import java.time.LocalDateTime;
 import java.util.UUID;
 
