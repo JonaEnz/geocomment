a4cba7c5c510f6461f97ac59da96fd8284dadf3d
JonaEnz <uhock@student.kit.edu>
2021-06-26 12:52:11 +0200

    login

--- frontend/geocomment/src/components/LoginView.tsx
+++ frontend/geocomment/src/components/LoginView.tsx
@@ -1,9 +1,16 @@
-import React from "react";
+import React, { FormEvent, useState } from "react";
 import { makeStyles } from "@material-ui/core/styles";
 import TextField from "@material-ui/core/TextField";
 import { Button, Grid, Paper } from "@material-ui/core";
 import VpnKeyIcon from "@material-ui/icons/VpnKey";
+import AccountCircle from "@material-ui/icons/AccountCircle";
+import Lock from "@material-ui/icons/Lock";
 import { useUserContext } from "../contexts/UserContext";
+import Input from "@material-ui/core/Input";
+import InputLabel from "@material-ui/core/InputLabel";
+import InputAdornment from "@material-ui/core/InputAdornment";
+import FormControl from "@material-ui/core/FormControl";
+import { Service } from "../api/services/Service";
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -16,41 +23,93 @@ const useStyles = makeStyles((theme) => ({
 
 function LoginView() {
   const { userCredentials, setUserCredentials } = useUserContext();
+
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+
   const classes = useStyles();
+
+  function submit(event: React.FormEvent<HTMLFormElement>) {
+    event.preventDefault();
+    Service.postService7({ email: username, password: password }).then(
+      (resp) => {
+        console.log(resp);
+      },
+      (error) => {
+        console.log(error);
+      }
+    );
+  }
+
   return (
     <div>
       <h1>LoginView</h1>
       <h3>email: {userCredentials.email}</h3>
       <h3>token: {userCredentials.token}</h3>
-      <Paper>
-        <form className={classes.root} autoComplete="off">
-          <Grid container>
-            <TextField required id="email" label="Email" />
-            <TextField
-              required
-              id="password"
-              label="Password"
-              type="password"
-            />
-            <Grid item alignContent="center">
-              <Button
-                variant="contained"
-                color="primary"
-                endIcon={<VpnKeyIcon />}
-                type="submit"
-                onClick={() =>
-                  setUserCredentials({
-                    email: "max.mustermann@gmail.com",
-                    token: "2dg638d3928h9283hd",
-                  })
-                }
+      <Grid container justify="center">
+        <Paper style={{ width: "300px" }}>
+          <Grid justify="center" alignItems="center" direction="column">
+            <form
+              className={classes.root}
+              autoComplete="off"
+              onSubmit={(e) => submit(e)}
+            >
+              <Grid
+                container
+                justify="center"
+                alignItems="center"
+                style={{ padding: "10px" }}
               >
-                login
-              </Button>
-            </Grid>
+                <FormControl>
+                  <TextField
+                    required
+                    id="email"
+                    label="Email"
+                    onChange={(e) => setUsername(e.target.value)}
+                    value={username}
+                    InputProps={{
+                      startAdornment: (
+                        <InputAdornment position="start">
+                          <AccountCircle />
+                        </InputAdornment>
+                      ),
+                    }}
+                  />
+                </FormControl>
+                <TextField
+                  required
+                  id="password"
+                  label="Password"
+                  type="password"
+                  value={password}
+                  onChange={(e) => setPassword(e.target.value)}
+                  InputProps={{
+                    startAdornment: (
+                      <InputAdornment position="start">
+                        <Lock />
+                      </InputAdornment>
+                    ),
+                  }}
+                />
+                <Button
+                  variant="contained"
+                  color="primary"
+                  endIcon={<VpnKeyIcon />}
+                  type="submit"
+                  onClick={() =>
+                    setUserCredentials({
+                      email: "max.mustermann@gmail.com",
+                      token: "2dg638d3928h9283hd",
+                    })
+                  }
+                >
+                  login
+                </Button>
+              </Grid>
+            </form>
           </Grid>
-        </form>
-      </Paper>
+        </Paper>
+      </Grid>
     </div>
   );
 }
